6f5396c923628eca82b2a08a666fa32606f0b240
xlx <xlx@cecs>
2012-11-25 18:20:38 +1100

    get rid of too much echo

--- imgtag/eval_TestCN5pr.m
+++ imgtag/eval_TestCN5pr.m
@@ -19,17 +19,20 @@ Y = [Y(1:NUMV, :); Y5p(1:NUMV, :)] ;
 % add features
 %Y = Yadd(1:NUMV, :); 
 
+echo off
 exp_setupTraining
 
+echo on
 %% learn the model
 fprintf('\n\n=========== K=%d, alpha=%f, num-v=%d ==============\n\n', K, alph, size(Y,1));        
 id_train = find(R(:));
 [U, V] = matchbox(R, X, Y, alph, 'indR', id_train, 'k', K, 'max_iter', max_iter, 'solver', 'lbfgs');
 
+echo off
 exp_runTest
 
 clear imgfeat imgid imglab X* R
 
 save(sav_file) 
 diary off
-echo off
+

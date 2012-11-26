60efd37bb0bd78d1957193f236c2bcce58bb6110
xlx <xlx@cecs>
2012-11-26 12:18:58 +1100

    finish svm2

--- imgtag/eval_Test_svm2.m
+++ imgtag/eval_Test_svm2.m
@@ -1,4 +1,4 @@
-
+% SVM baseline, with no hyper par search
 
 eval_str = 'eval_Testsvm_' ;
 
@@ -6,6 +6,11 @@ exp_envsetup
 exp_setparams
 
 whos
+
+num_data_sample = inf;
+
+disp(eval_str)
+disp(num_data_sample)
 
 %matlabpool local 6
 
@@ -33,32 +38,15 @@ ntag = size(imglab, 2) ;
 tagcnt = full(sum(imglab,2));
 nlen = length(tagcnt);
 
-X = imgfeat'; %([id1;id2], :)';
-
-%save(sav_file) 
-
-fprintf('\n\n=========== K=%d, alpha=%f, num-v=%d ==============\n\n', K, alph, size(Y,1));
-
-%gam = 0 ;
-svm_param = cell(ntag, 1);
-parfor j = 1 : ntag
-    fprintf(1, 'Learning linear svm for tag #%d, "%s"\n', j, tag_dim{j});
-    % linear SVM baseline
-    curid = (imglab(:,j)~=0) ;
-    %[mod_final, ~] = cv_svm_wrapper2(1.*imglab(:,j), X', ...
-    %    'do_normalize', true, 'kernel_type', 0, 'gam', 0, 'Cpen', 10.^(-1:3), 'neg_pos_ratio', 8);
-    svm_options = '-t 0 -c 1 -q';
-    train_label = 1.*imglab(:,j) ;
-    out_flag = sample_pos_neg(imglab(:,j), neg_pos_ratio, max_num_pos, max_num_neg);
-    train_label(train_label==0) = -1;
-    train_data = X(:, out_flag)' ;
-    lsvm = svmtrain(train_label(out_flag), train_data, svm_options);
-    svm_param{j} = lsvm;
-    
+% subselect some data
+if ~isinf(num_data_sample)
+    idx = rand_idx(nlen, num_data_sample) ;
+else
+    idx = 1 : nlen;
 end
-save(sav_file, 'svm_param', 'tag_dim') ;
 
-%% evaluate on Test set
+R = imglab(idx, :);
+X = imgfeat(idx, :)';
 
 %% load entire test data list and labels
 load(fullfile(data_dir, 'TrainTest_Label.mat'), 'tag81', 'test_label', 'test_idx_map') 
@@ -76,8 +64,8 @@ load(fullfile(data_dir, 'TrainTest_Label.mat'), 'tag81', 'test_label', 'test_idx
 %   tr_tag81           161789x81               2249456  double            sparse    
 %   train_idx_map            -                     112  containers.Map              
 %   train_label        161789x81               4824000  double            sparse    
-%   train_tag_1k       161789x1000            14971624  double            sparse    
-
+%   train_tag_1k       161789x1000            14971624  double
+%   sparse    
 
 %% setup X
 feat_data_mat = fullfile(data_dir, 'Test_feat_objbank.mat');
@@ -92,11 +80,34 @@ whos imgfeat imgid imglab
 %% now compare classification
 Xtest = imgfeat';
 Rtest = zeros(size(imglab));
-parfor j = 1 : ntag
-    % evaluate
+
+fprintf('\n\n=========== K=%d, alpha=%f, num-v=%d ==============\n\n', K, alph, size(Y,1));
+
+%gam = 0 ;
+svm_param = cell(ntag, 1);
+%parfor j = 1 : ntag
+for j = 1 : ntag
+    fprintf(1, 'Learning linear svm for tag #%d, "%s"\n', j, tag_dim{j});
+    % linear SVM baseline
+    %[mod_final, ~] = cv_svm_wrapper2(1.*imglab(:,j), X', ...
+    %    'do_normalize', true, 'kernel_type', 0, 'gam', 0, 'Cpen', 10.^(-1:3), 'neg_pos_ratio', 8);
+    svm_options = '-t 0 -c 1 -q';
+    train_label = 1.*(R(:,j)~=0) - 1.*(R(:,j)==0)  ;
+    
+    out_flag = sample_pos_neg(train_label, neg_pos_ratio, max_num_pos, max_num_neg);
+    
+    train_data = X(:, out_flag)' ;
+    lsvm = svmtrain(train_label(out_flag), train_data, svm_options);
+    svm_param{j} = lsvm;
+    
     [~, ~, Rtest(:, j)] = svmpredict(imglab(:,j), Xtest', svm_param{j});
-    %pk = compute_perf(Rtest(:, j), 1.*full(imglab(:,j)), 'store_raw_pr', 2);
+    pk = compute_perf(Rtest(:, j), 1.*full(imglab(:,j)), 'store_raw_pr', 2);
+    
+    fprintf(1, 'tag%d "%s" test ap: %0.4f, auc: %0.4f, F1: %0.4f\n\n', j, tag_dim{j}, pk.ap, pk.auc, pk.f1);
+    
 end
+save(sav_file, 'svm_param', 'tag_dim') ;
+
 %Rtest = Xtest'*U'*V*Y;
 p_all = compute_perf(Rtest(:), 1.*full(imglab(:)), 'store_raw_pr', 2);
 
@@ -126,9 +137,13 @@ end
 fprintf(1, ' MAP on allTest: \n\tmatchbox %0.4f, knn %0.4f, prior %0.4f\n',...
     mean(ap_concept), mean(prior_concept));
 
-disp(tag_dim)
-disp([ap_concept, prior_concept] )
+[~, ik] = sort( prior_concept, 'descend' );
+for i = 1 : ntag
+    jj = ik(i);
+    fprintf(1, '%0.4f\t %0.4f\t %0.4f\t %s \n', ap_concept(jj, :), prior_concept(jj), tag_dim{jj});
+end
 
+clear imgfeat imgid imglab X* R
 save(sav_file) 
 
 matlabpool close

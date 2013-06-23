746e04009367ec1792e725f2a87d5fdf4441e5a6
xlx <xlx@cecs>
2013-06-23 18:55:00 +1000

    combine shelve file access

--- TagNet/compute_tag_dispersion.py
+++ TagNet/compute_tag_dispersion.py
@@ -11,13 +11,12 @@ from optparse import OptionParser
 
 PRINT_FLAG = 1
 
-def get_wn_similarity(shelve_name, wn_list, offset_dict):
+def get_wn_similarity(sim, wn_list, offset_dict):
     idx_list = gen_pairs_idx(len(wn_list))
     wn_sim = []
     cache_cnt = 0
     new_cnt = 0
     
-    sim = shelve.open(shelve_name, writeback=False)
     for i, j in idx_list:
         if wn_list[i] < wn_list[j]:
             k1 = offset_dict[wn_list[i]]
@@ -43,8 +42,6 @@ def get_wn_similarity(shelve_name, wn_list, offset_dict):
             new_cnt += 1
         
         wn_sim.append(s)
-    
-    sim.close()
     
     if PRINT_FLAG:
         tt = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
@@ -75,7 +72,8 @@ def store_wn_lookup():
     pickle.dump(offset_dict, open('/Users/xlx/Documents/proj/imgnet-flickr/db3/wn_offset_dict.pickle', 'wb'))
 
 def compute_dispersion(argv):
-
+    global PRINT_FLAG
+    
     if len(argv)<2:
         argv = ['-h']
     
@@ -84,20 +82,19 @@ def compute_dispersion(argv):
     parser.add_option('-o', '--out_dir', dest='out_dir', default="", help='output dir')
     parser.add_option('-f', '--db_file', dest='db_file', default="wordnet_tag.db", help='dictionary')
     parser.add_option('-s', '--shelve_file', dest='shelve_file', default="tag_dispersion.shelve", help='name of the cache file')
-    parser.add_option('', '--output_file', dest='output_file', default="tag_features.csv", help='name of the ouptut file')
+    parser.add_option('', '--output_file', dest='output_file', default="tag_dispersion.csv", help='name of the ouptut file')
     
     parser.add_option('', '--offset_file', dest='offset_file', default='wn_offset_dict.pickle' )
     
     parser.add_option('', '--start_idx', dest='start_idx', type='int', default=0, help='start index of tags')
-    parser.add_option('', '--end_idx', dest='end_idx', type='int', default=10)
+    parser.add_option('', '--end_idx', dest='end_idx', type='int', default=-1)
     parser.add_option('', '--topK', dest='topK', type='int', default=-1, help="only consider topK wn per tag, to speed things up")
     
     (opts, __args) = parser.parse_args(argv)
     
     if not opts.out_dir:
         opts.out_dir = opts.db_dir
-    
-    shelve_file = os.path.join(opts.out_dir, opts.shelve_file) 
+        
     out_file = os.path.join(opts.out_dir, opts.output_file)
     fo = open(out_file, 'wt')
     
@@ -109,12 +106,17 @@ def compute_dispersion(argv):
     
     tlist = cursor.execute("SELECT DISTINCT tag FROM wn_tag").fetchall() # list of tags
     
+    shelve_file = os.path.join(opts.out_dir, opts.shelve_file) 
+    sim = shelve.open(shelve_file, writeback=False)
+    
     cnt = 0
     nt = len(tlist)
     
     for tag in tlist:
         cnt += 1
-        if cnt < opts.start_idx or cnt > opts.end_idx:
+        if cnt < opts.start_idx: 
+            continue
+        if opts.end_idx > 0 and cnt > opts.end_idx:
             continue
         # get the list of associated wns and their count
         tag = tag[0]
@@ -128,24 +130,30 @@ def compute_dispersion(argv):
         wn_list = tmp[0]
         wn_count = tmp[1]
         
-        PRINT_FLAG = (cnt % 20 == 0)
-         
+        PRINT_FLAG = (cnt % 10 == 0)
+        
+        numwn = len(wn_list)
         idx_list = gen_pairs_idx(len(wn_list))
+        
         if idx_list:
-            wn_sim = get_wn_similarity(shelve_file, wn_list, offset_dict)
+            wn_sim = get_wn_similarity(sim, wn_list, offset_dict)
             
             pair_total = map(lambda u:  1.*wn_count[u[0]]*wn_count[u[1]], idx_list)
             npair = sum(pair_total)
+            
             avg_sim = sum(map(lambda a,b: a*b, pair_total, wn_sim)) / npair
             
             if PRINT_FLAG:
                 tt = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-                print "%s tag#%d/%d %s, sim: %0.4f, #pairs %d " % (tt, cnt, nt, tag, avg_sim, npair)
-            fo.write("%s,%0.4f\n" % (tag, avg_sim) )
+                print "%s %d/%d %s\t sim %0.4f\t #synsets %d\t #pairs %d" % (tt, cnt, nt, tag, avg_sim, numwn, npair)
+            fo.write("%s,%d,%0.4f\n" % (tag, numwn, avg_sim) )
         else:
-            tt = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-            print "%s tag#%d/%d %s, empty" % (tt, cnt, nt, tag)
+            if PRINT_FLAG:
+                tt = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+                print "%s tag#%d/%d %s, empty" % (tt, cnt, nt, tag)
+            fo.write("%s,%d,%0.4f\n" % (tag, numwn, 0.) )
     
+    sim.close()
     fo.close()
     conn.close()
     return
